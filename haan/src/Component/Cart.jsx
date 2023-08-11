80b2d77a78ad72e966ce3074669fdbecec3da5ab
Lokesh <lokeshk.charan3@gmail.com>
2023-08-12 01:25:03 +0530

    almost complete

--- haan/src/Component/Cart.jsx
+++ haan/src/Component/Cart.jsx
@@ -6,8 +6,11 @@ import gstImg from '../Media/gst.png';
 
 const Cart = () => {
   const [productsData, setProductsData] = useState([]);
+
   const [quantity, setQuantity] = useState(1);
   
+  const [isOverlayOpen, setIsOverlayOpen] = useState(false);
+
   const getData = async () => {
     try {
       let res = await axios.get(`https://haanproject.onrender.com/products`);
@@ -57,34 +60,58 @@ const Cart = () => {
   window.upScroll = upScroll;
   window.downScroll = downScroll;
 
+  const openNav = () => {
+    setIsOverlayOpen(true);
+  };
+
+  const closeNav = () => {
+    setIsOverlayOpen(false);
+  };
+
   return (
-    <div className={styles.wholeCart}>
-      <div className={styles.initialCart}>
-        <img className={styles.image} src={gstImg} alt="gst"/>
-      </div>
-      <div className={`${styles.mainCartDiv} ${styles.scrollableContainer}`}>
-        {productsData.map((ele) => (
-          <div className={styles.cardDiv} key={ele.id}>
-            <div className={styles.imgDiv}>
-              <img style={{ width: '120px', height: '120px' }} src={ele.img} alt='product' />
-            </div>
-            <div className={styles.detailsDiv}>
-              <h3>{ele.name}</h3>
-              <p>{ele.price} Rs. </p>
-              <div className={styles.btnDiv}>
-                <button onClick={handleDec} disabled={quantity === 1} style={{fontSize:'larger'}}>-</button>
-                <p>{quantity}</p>
-                <button onClick={handleInc} disabled={quantity === 10} style={{fontSize:'larger'}}>+</button>
+    <div>
+      {/* Button to open the overlay */}
+      <span className={styles.openBtn} onClick={openNav}>Cart &#9776;</span>
+
+      {/* Overlay */}
+      {isOverlayOpen && (
+        <div className={styles.overlay}>
+          <div className={styles.overlayContent}>
+          <button className={styles.closeBtn} style={{fontSize:'larger'}} onClick={closeNav}>&times;</button>
+
+          {/* here is cart div */}
+            <div className={styles.wholeCart}>
+              <div className={styles.initialCart}>
+                <img className={styles.image} src={gstImg} alt="gst"/>
               </div>
-              <div><hr className={styles.hrLine}/></div>
+              <div className={`${styles.mainCartDiv} ${styles.scrollableContainer}`}>
+                {productsData.map((ele) => (
+                  <div className={styles.cardDiv} key={ele.id}>
+                    <div className={styles.imgDiv}>
+                      <img style={{ width: '120px', height: '120px' }} src={ele.img} alt='product' />
+                    </div>
+                    <div className={styles.detailsDiv}>
+                      <h3>{ele.name}</h3>
+                      <p>{ele.price} Rs. </p>
+                      <div className={styles.btnDiv}>
+                        <button onClick={handleDec} disabled={quantity === 1} style={{fontSize:'larger'}}>-</button>
+                        <p>{quantity}</p>
+                        <button onClick={handleInc} disabled={quantity === 10} style={{fontSize:'larger'}}>+</button>
+                      </div>
+                      <div><hr className={styles.hrLine}/></div>
+                    </div>
+                  </div>
+                ))}
+              </div>
+              <div className={styles.checkoutDiv}>
+                <h2>Total : {calculateTotal()} Rs.</h2>
+                <button>Checkout</button>
+              </div>
             </div>
-          </div>
-        ))}
-      </div>
-      <div className={styles.checkoutDiv}>
-        <h2>Total : {calculateTotal()} Rs.</h2>
-        <button>Checkout</button>
-      </div>
+
+           </div>
+        </div>
+      )}
     </div>
   );
 };
