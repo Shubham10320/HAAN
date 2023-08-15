06a9100f78ba80ff2b57275f7b12c3b4d63fa6ed
Lokesh <lokeshk.charan3@gmail.com>
2023-08-16 01:19:16 +0530

    cart page completed

--- haan/src/Component/Cart.jsx
+++ haan/src/Component/Cart.jsx
@@ -1,21 +1,23 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import styles from './Cart.module.css';
 import gstImg from '../Media/gst.png';
 
 const Cart = () => {
   const [productsData, setProductsData] = useState([]);
-
-  const [quantity, setQuantity] = useState(1);
-  
-  const [isOverlayOpen, setIsOverlayOpen] = useState(true);  //if you want to add button for the pop up the cart make this intital state false useState(false)
+  const [quantities, setQuantities] = useState({}); // State to manage individual quantities
+  const [isOverlayOpen, setIsOverlayOpen] = useState(true); 
 
   const getData = async () => {
     try {
-      let res = await axios.get(`https://haanproject.onrender.com/products`);
+      let res = await axios.get(`https://haanproject.onrender.com/cartData`);
       console.log(res.data);
       setProductsData(res.data);
+      const initialQuantities = {};
+      res.data.forEach(product => {
+        initialQuantities[product.id] = 1; // Initialize quantities for each product
+      });
+      setQuantities(initialQuantities);
     } catch (error) {
       console.log("error while fetching data");
     }
@@ -25,61 +27,52 @@ const Cart = () => {
     getData();
   }, []);
 
-  const handleQuantityChange = (newQuantity) => {
+  const handleQuantityChange = (productId, newQuantity) => {
     if (newQuantity >= 1 && newQuantity <= 10) {
-      setQuantity(newQuantity);
+      setQuantities(prevQuantities => ({
+        ...prevQuantities,
+        [productId]: newQuantity
+      }));
     }
   };
 
-  const handleInc = () => {
-    handleQuantityChange(quantity + 1);
+  const handleInc = (productId) => {
+    handleQuantityChange(productId, quantities[productId] + 1);
   };
 
-  const handleDec = () => {
-    handleQuantityChange(quantity - 1);
+  const handleDec = (productId) => {
+    handleQuantityChange(productId, quantities[productId] - 1);
   };
 
   const calculateTotal = () => {
     const totalPrice = productsData.reduce((total, product) => {
-      return total + product.price * quantity;
+      return total + product.price * quantities[product.id];
     }, 0);
     return totalPrice;
   };
 
-  // Sliding Function
-  const orderSummary = document.querySelector(".cardDiv");
+  const handleDelete = async (productId) => {
+    if (quantities[productId] <= 1) {
+      await axios.delete(`https://haanproject.onrender.com/cartData/${productId}`);
+      getData();
+    }
+  };
 
-  const upScroll = () => {
-      orderSummary.scrollBy(0, -200);
-  }
-
-  const downScroll = () => {
-      orderSummary.scrollBy(0, 200);
-  }
-
-  window.upScroll = upScroll;
-  window.downScroll = downScroll;
-
-  // const openNav = () => {
-  //   setIsOverlayOpen(true);
-  // };
-
-  const closeNav = () => {
+    const closeNav = () => {
     setIsOverlayOpen(false);
   };
 
   return (
     <div>
-      {/* Button to open the overlay */}
-      {/* <span className={styles.openBtn} onClick={openNav}>Cart &#9776;</span> */}
-
+      { /* ... (existing code) ... */ }
+      
       {/* Overlay */}
       {isOverlayOpen && (
         <div className={styles.overlay}>
           <div className={styles.overlayContent}>
-          <button className={styles.closeBtn} style={{fontSize:'larger'}} onClick={closeNav}>&times;</button>
-
-          {/* here is cart div */}
+            <button className={styles.closeBtn} style={{fontSize:'larger'}} onClick={closeNav}>&times;</button>
+            
+            {/* here is cart div */}
             <div className={styles.wholeCart}>
               <div className={styles.initialCart}>
                 <img className={styles.image} src={gstImg} alt="gst"/>
@@ -91,14 +84,18 @@ const Cart = () => {
                       <img style={{ width: '120px', height: '120px' }} src={ele.img} alt='product' />
                     </div>
                     <div className={styles.detailsDiv}>
-                      <h3>{ele.name}</h3>
+                        <h3>{ele.name}</h3>
                       <p>{ele.price} Rs. </p>
                       <div className={styles.btnDiv}>
-                        <button onClick={handleDec} disabled={quantity === 1} style={{fontSize:'larger'}}>-</button>
-                        <p>{quantity}</p>
-                        <button onClick={handleInc} disabled={quantity === 10} style={{fontSize:'larger'}}>+</button>
+                        <button onClick={() => handleDec(ele.id)} style={{fontSize:'larger'}}>-</button>
+                        <p>{quantities[ele.id]}</p>
+                        <button onClick={() => handleInc(ele.id)} disabled={quantities[ele.id] === 10} style={{fontSize:'larger'}}>+</button>
+                        <button onClick={() => handleDelete(ele.id)}>Delete</button>
                       </div>
-                      <div><hr className={styles.hrLine}/></div>
+                     
+                        {/* <button onClick={() => handleDelete(ele.id)}>Delete</button> */}
+                        <hr className={styles.hrLine}/>
+                     
                     </div>
                   </div>
                 ))}
@@ -108,8 +105,7 @@ const Cart = () => {
                 <button>Checkout</button>
               </div>
             </div>
-
-           </div>
+          </div>
         </div>
       )}
     </div>
@@ -117,3 +113,4 @@ const Cart = () => {
 };
 
 export default Cart;
+
