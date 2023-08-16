7ff464b531952557e7dbbf8ac240f0cdc57dd2bd
MohdZaid96 <i.m.mdz.1996@gmail.com>
2023-08-16 19:55:37 +0530

    Completed

---
+++ haan/src/Component/Cart.jsx
@@ -0,0 +1,120 @@
+import React, { useState, useEffect ,useContext} from 'react';
+import axios from 'axios';
+import styles from './Cart.module.css';
+import gstImg from '../Media/gst.png';
+import { useNavigate } from 'react-router-dom';
+import {AuthContext} from "../Context/AuthContextApi"
+
+const Cart = () => {
+  const navigate=useNavigate();
+  const { authState } = useContext(AuthContext);
+  const [productsData, setProductsData] = useState([]);
+  const [quantities, setQuantities] = useState({}); // State to manage individual quantities
+  const [isOverlayOpen, setIsOverlayOpen] = useState(true); 
+
+  const getData = async () => {
+    try {
+      let res = await axios.get(`https://haanproject.onrender.com/cartData`);
+      console.log(res.data);
+      setProductsData(res.data);
+      const initialQuantities = {};
+      res.data.forEach(product => {
+        initialQuantities[product.id] = 1; // Initialize quantities for each product
+      });
+      setQuantities(initialQuantities);
+    } catch (error) {
+      console.log("error while fetching data");
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const handleQuantityChange = (productId, newQuantity) => {
+    if (newQuantity >= 1 && newQuantity <= 10) {
+      setQuantities(prevQuantities => ({
+        ...prevQuantities,
+        [productId]: newQuantity
+      }));
+    }
+  };
+
+  const handleInc = (productId) => {
+    handleQuantityChange(productId, quantities[productId] + 1);
+  };
+
+  const handleDec = (productId) => {
+    handleQuantityChange(productId, quantities[productId] - 1);
+  };
+
+  const calculateTotal = () => {
+    const totalPrice = productsData.reduce((total, product) => {
+      return total + product.price * quantities[product.id];
+    }, 0);
+    return totalPrice;
+  };
+
+  const handleDelete = async (productId) => {
+    if (quantities[productId] <= 1) {
+      await axios.delete(`https://haanproject.onrender.com/cartData/${productId}`);
+      getData();
+    }
+  };
+
+    const closeNav = () => {
+    setIsOverlayOpen(false);
+  };
+
+  return (
+    <div>
+      { /* ... (existing code) ... */ }
+      
+      {/* Overlay */}
+      {isOverlayOpen && (
+        <div className={styles.overlay}>
+          <div className={styles.overlayContent}>
+            <button className={styles.closeBtn} style={{fontSize:'larger'}} onClick={closeNav}>&times;</button>
+            
+            {/* here is cart div */}
+            <div className={styles.wholeCart}>
+              <div className={styles.initialCart}>
+                <img className={styles.image} src={gstImg} alt="gst"/>
+              </div>
+              <div className={`${styles.mainCartDiv} ${styles.scrollableContainer}`}>
+                {productsData.map((ele) => (
+                  <div className={styles.cardDiv} key={ele.id}>
+                    <div className={styles.imgDiv}>
+                      <img style={{ width: '120px', height: '120px' }} src={ele.img} alt='product' />
+                    </div>
+                    <div className={styles.detailsDiv}>
+                        <h4>{ele.name}</h4>
+                      <p>{ele.price} Rs. </p>
+                      <div className={styles.btnDiv}>
+                        <button onClick={() => handleDec(ele.id)} style={{fontSize:'larger'}}>-</button>
+                        <p style={{marginBottom:'0'}}>{quantities[ele.id]}</p>
+                        <button onClick={() => handleInc(ele.id)} disabled={quantities[ele.id] === 10} style={{fontSize:'larger'}}>+</button>
+                        <button onClick={() => handleDelete(ele.id)}>Delete</button>
+                      </div>
+                     
+                        {/* <button onClick={() => handleDelete(ele.id)}>Delete</button> */}
+                        <hr className={styles.hrLine}/>
+                     
+                    </div>
+                  </div>
+                ))}
+              </div>
+              <div className={styles.checkoutDiv}>
+                <h4>Total : {calculateTotal()} Rs.</h4>
+                <button id='checkoutBtn' onClick={()=>{  navigate(authState.isAuth?"/payment":"/login")}}>Checkout</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Cart;
+
